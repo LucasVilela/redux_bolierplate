5c0013db5fcdcb9e2c9ac2911f39ecfc1dbae79b
Lucas Vilela <fleminglucas@gmail.com>
2017-09-14 16:40:34 +1000

    Basic create-app and redux boilerplate

--- src/store.js
+++ src/store.js
@@ -3,7 +3,10 @@ import { createStore } from "redux";
 import reducer from "./reducers/reducer";
 
 const defaultState = {
-  test: []
+  test: {
+    data: 1,
+    text: "lorem"
+  }
 };
 const store = createStore(reducer, defaultState);
 
